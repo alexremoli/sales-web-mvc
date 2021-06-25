e674b342a58bba894b0aef015a68257387074ea0
alexremoli <alex.bushido@hotmail.com>
2021-06-25 10:52:51 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Migrations/20210625134833_OtherEntities.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class OtherEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Salary",
+                table: "Seller",
+                newName: "BaseSalary");
+
+            migrationBuilder.RenameColumn(
+                name: "Date",
+                table: "Seller",
+                newName: "BirthDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "Seller",
+                newName: "Date");
+
+            migrationBuilder.RenameColumn(
+                name: "BaseSalary",
+                table: "Seller",
+                newName: "Salary");
+        }
+    }
+}
